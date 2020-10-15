6dff0bfc0e1a0ac641d7c079cdae7408e927ab2a
Karol <KarolKaczorowskic.dev@gmail.com>
2020-10-15 14:01:33 +0200

    Updates

---
+++ precache-manifest.85d642c8bf454e70e848b4654fb73edb.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871e029daed8326c2f93f86e6a180c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc224197fbc868e8175",
+    "url": "/static/css/main.1b194ff7.chunk.css"
+  },
+  {
+    "revision": "630e0f3e4c3a30a7c6dc",
+    "url": "/static/js/2.b9e481c9.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/static/js/2.b9e481c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc224197fbc868e8175",
+    "url": "/static/js/main.80a3392c.chunk.js"
+  },
+  {
+    "revision": "8c0fdaefb3abb2b473f3",
+    "url": "/static/js/runtime-main.6c3137ea.js"
+  },
+  {
+    "revision": "df183ad0d920e01cd5bfa75bd9417a3c",
+    "url": "/static/media/HR.df183ad0.jpg"
+  },
+  {
+    "revision": "31f22a66b7eaacfbd53a39f0fa74fb1c",
+    "url": "/static/media/Interview.31f22a66.jpg"
+  },
+  {
+    "revision": "d67e16b7cf27866c580281d123ff6027",
+    "url": "/static/media/PhoneScreen.d67e16b7.jpg"
+  },
+  {
+    "revision": "8c35018cab17300eca59ee52430a2a71",
+    "url": "/static/media/SendCV.8c35018c.jpg"
+  },
+  {
+    "revision": "aad719510c90eb7b60284d779f08f90e",
+    "url": "/static/media/TechnicalAssesment.aad71951.jpg"
+  },
+  {
+    "revision": "17df383754e248b8dca57d0538d97b07",
+    "url": "/static/media/Work.17df3837.jpg"
+  },
+  {
+    "revision": "8812e5a261a0f05c1a36d3672944d9cc",
+    "url": "/static/media/aboutMain.8812e5a2.jpg"
+  },
+  {
+    "revision": "4db8c2780adeeb17b60c7b1e7425b9bd",
+    "url": "/static/media/aboutOffice.4db8c278.jpg"
+  },
+  {
+    "revision": "ad00c840df73c0fd2954fff482cc529e",
+    "url": "/static/media/aboutWorkflow.ad00c840.jpg"
+  },
+  {
+    "revision": "33dac30cb125444ba3ff43e36713e312",
+    "url": "/static/media/ai.33dac30c.svg"
+  },
+  {
+    "revision": "1be20c3aa503234b8b54f2f23f3c3f08",
+    "url": "/static/media/careersMain.1be20c3a.jpg"
+  },
+  {
+    "revision": "78c38afc784ffd490b7c1289b5421562",
+    "url": "/static/media/design.78c38afc.svg"
+  },
+  {
+    "revision": "56d0c951f7e16ea03f65e09b043106be",
+    "url": "/static/media/estimateProject.56d0c951.jpg"
+  },
+  {
+    "revision": "43a3a3f79da7ebd17f9de6175987663b",
+    "url": "/static/media/handShake.43a3a3f7.jpg"
+  },
+  {
+    "revision": "5a98fb37d5cd5fdc60b7ac8d8e1fc8c6",
+    "url": "/static/media/mobile.5a98fb37.svg"
+  },
+  {
+    "revision": "8fa4da57b69321e4c41ffcf8a34328cc",
+    "url": "/static/media/servicesMain.8fa4da57.jpg"
+  },
+  {
+    "revision": "aa7c14202f841c91728f78fba43de078",
+    "url": "/static/media/web.aa7c1420.svg"
+  }
+]);
